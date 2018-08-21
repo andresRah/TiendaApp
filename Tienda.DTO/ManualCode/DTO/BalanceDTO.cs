4a284180fd7af86f73ce89fe6333f5e05faf4642
andres.arevalopar <andres.arevalopar@outlook.es>
2018-08-20 22:50:13 -0500

    First step Service Layer

---
+++ Tienda.DTO/ManualCode/DTO/BalanceDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Tienda.DTO.ManualCode.DTO
+{
+    using System;
+    using System.ComponentModel;
+
+    public partial class BalanceDTO
+    {
+        [DisplayName("BalanceId")]
+        public int BalanceId { get; set; }
+    }
+}
