32c6d54a3c1294f605602dd1f0b5f04ec4fc300d
andres.arevalopar <andres.arevalopar@outlook.es>
2018-08-24 16:15:00 -0500

    Initial controllerBase for Product

---
+++ TiendaApp/Controllers/ProductContoller.cs
@@ -0,0 +1,29 @@
+﻿namespace TiendaApp.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class ProductContoller : ControllerBase
+    {
+        static IProductServices _services;
+
+        public ProductContoller()
+        {
+            _services = new ProductServices();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<DataSourceResult>> Get([DataSourceRequest] DataSourceRequest request)
+        {
+            DataSourceResult result = _services.GetAll(request);
+            return result;
+        }
+
+
+
+    }
+}
